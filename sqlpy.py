8f979ea4042024d92a3ad6e56f7e37c196c78838
Hamza Ali <hamzaali151214@hotmail.com>
2019-01-24 08:48:25 +0300

    fixed some stuff

--- sqlpy.py
+++ sqlpy.py
@@ -1,85 +1,45 @@
 '''
-python program for running sqlite3 queries
+python3 function for running sqlite3 queries
 '''
 import sqlite3
-def selectsearch(query):
-    '''
-    Docstring:
-    This function returns 0 if SELECT statement else -1
-    '''
-    result=query.find("SELECT")*query.find("Select")*query.find("select")
-    return result
-def select_sql(selectquery,dbname):
-    '''
-    Docstring:
-    This function is used to run queries to SELECT features
-    and displays the result. 
-    '''
-    conn=sqlite3.connect(dbname)
-    cursor=conn.execute(selectquery)
-    for row in cursor:
-        print(row)
 
-
-def script(sql_string,dbname):
-    '''
-    Docstring:
-    This function runs SQL queries on a database called somedatabase.db
-    script(querystring).
-    '''
-    check=selectsearch(sql_string)
-    if check==0:
-        select_sql(sql_string,dbname)
-    elif check==-1:
-        conn=sqlite3.connect(dbname)
-        conn.execute(sql_string)
-        conn.commit()
-    else:
-        print("error lol")
 #remember to check dbname when connecting to database
 
-def runsqlquery():
+def runsqlquery(database_name1):
     '''
     Docstring:
     Enter query mode in sqlite3
+    runsqlquery(database_name)
+    >query
     '''
-    sql_query="something"
-    print("1.create database (use this if running for the first time)")
-    print("2.use existing database")
-    choice=int(input('>'))
-    if choice==1:
-        print("Please enter the name of the database that you wish to create followed by .db")
-        database_name1=input('>')
-        while sql_query!="quit":
-            print("Please enter your query ('quit' to quit query mode)")
-            query=input('>')
-            if query=="QUIT" or query=="quit":
-             break
+    query="placeholder"
+    print(f"sqlite3 query mode. Database:{database_name1} ('quit' to exit query mode)")
+    while query!="quit":
+        query=input('>')
+        if query=="QUIT" or query=="quit":
+            print("Exiting query mode...")
+            break
+            #try getting rid of break
+        check=query.find("SELECT")*query.find("Select")*query.find("select")
+        if check==0:
+            conn=sqlite3.connect(database_name1)
+            cursor=conn.execute(query)
+            for row in cursor:
+                print(row)
+        else:
+            conn=sqlite3.connect(database_name1)
+            conn.execute(query)
+            conn.commit()
 
-            script(query,database_name1)
-        print("Exited query mode.")
 
-    elif choice==2:
-        print("Please enter the name of the database that you created .db")
-        database_name=input('>')
-        while sql_query!="quit":
-            print("Please enter your query ('quit' to quit query mode)")
-            query=input('>')
-            if query=="quit" or query=="QUIT":
-                break
-            script(query,database_name)
-            
-
-        #exited query loop
-        print("Exited query mode")
-    else:
-        print("Wrong choice.")
-        runsqlquery()
-
-        
-        
 #<module> begins
-runsqlquery()
+print("Enter a database")
+db=input('>')
+help(runsqlquery)
+runsqlquery(db)
+
+
+
 
 
 
